42db2922709515605b7a549243bc77bb5c0a232e
Philip Laureano <philip.laureano@gmail.com>
2014-07-29 22:40:33 +1000

    Added support for generating Func<> instances from constructors at runtime using the Constructor.CallTo<> method, as well as added the BindToArray extension method, which binds an argument list to an array argument

---
+++ Functify/ArrayToExplicitArgumentConversionExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functify
+{
+    public static class ArrayToExplicitArgumentConversionExtensions
+    {
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1)
+        {
+            return () => builderFunc(new[] { arg1 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2)
+        {
+            return () => builderFunc(new[] { arg1, arg2 });
+        }
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3 });
+        }
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7 });
+        }
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 });
+        }
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 });
+        }
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9, T1 arg10)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10 });
+        }
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9, T1 arg10, T1 arg11)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9, T1 arg10, T1 arg11, T1 arg12)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9, T1 arg10, T1 arg11, T1 arg12, T1 arg13)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9, T1 arg10, T1 arg11, T1 arg12, T1 arg13, T1 arg14)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9, T1 arg10, T1 arg11, T1 arg12, T1 arg13, T1 arg14, T1 arg15)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15 });
+        }
+
+        public static Func<TResult> BindToArray<T1, TResult>(this Func<T1[], TResult> builderFunc, T1 arg1, T1 arg2, T1 arg3, T1 arg4, T1 arg5, T1 arg6, T1 arg7, T1 arg8, T1 arg9, T1 arg10, T1 arg11, T1 arg12, T1 arg13, T1 arg14, T1 arg15, T1 arg16)
+        {
+            return () => builderFunc(new[] { arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16 });
+        }
+    }
+}
---
+++ Functify/Constructor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using LinFu.Finders;
+using LinFu.Finders.Interfaces;
+
+namespace Functify
+{
+    public class Constructor
+    {
+        public static Func<object[], TResult> CallTo<TResult>()
+        {
+            const BindingFlags Flags = BindingFlags.Public | BindingFlags.Instance;
+
+            Func<MethodBase, int, Type, bool> matchesParameterType = (method, index, type) =>
+            {
+                var parameters = method.GetParameters();
+                var parameter = index < parameters.Length ? parameters[index] : null;
+                return parameter != null && type.IsAssignableFrom(parameter.ParameterType);
+            };
+
+            var constructors = typeof(TResult).GetConstructors(Flags);
+            return args =>
+            {
+                var candidateConstructors = constructors.AsFuzzyList();
+                var expectedArgumentCount = args != null ? args.Length : 0;
+
+                // Match the argument count
+                var arguments = args ?? new object[0];
+                for (var i = 0; i < expectedArgumentCount; i++)
+                {
+                    var currentArgument = arguments[i];
+
+                    // Match the argument types
+                    var currentIndex = i;
+                    if (currentArgument != null)
+                        candidateConstructors.AddCriteria(m => matchesParameterType(m, currentIndex, currentArgument.GetType()), CriteriaType.Critical);
+                }
+
+                var bestMatch = candidateConstructors.BestMatch();
+                if (bestMatch == null)
+                    throw new MissingMethodException(string.Format("Unable to find a suitable constructor for type '{0}'", typeof(TResult)));
+
+                var constructor = bestMatch.Item;
+                return (TResult)constructor.Invoke(args);
+            };
+        }
+    }
+}
